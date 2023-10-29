ff9e2881efa32f7f60708b14903aeabc09c98fe8
asasinmode <skin9470@gmail.com>
2023-10-29 12:12:49 +0100

    feat: rework toggle style

--- components/SideToggle.vue
+++ components/SideToggle.vue
@@ -6,30 +6,45 @@ defineProps<{
 	title: string;
 	falseLabel: string;
 	trueLabel: string;
+	falseClass?: string;
+	trueClass?: string;
 }>();
 
 const model = defineModel<boolean>({ required: true });
 </script>
 
 <template>
-	<label :for="id" class="leading-7 text-5 block text-[var(--vp-c-text-2)]">
-		{{ title }}:
-		<div class="inline-flex items-center gap-1 text-[var(--vp-c-text-1)]">
+	<div class="grid grid-cols-[min-content_min-content_auto] gap-x-2 text-[var(--vp-c-text-2)] w-fit">
+		<label class="col-span-full" :for="id">{{ title }}:</label>
+		<label
+			class="cursor-pointer transition-250 transition-color"
+			:class="model ? 'text-[var(--vp-c-text-2)]' : 'text-[var(--vp-c-text-1)]'"
+			@click.stop="model = false"
+		>
 			{{ falseLabel }}
-			<button
-				:id="id"
-				class="relative rounded-[0.6875rem] inline w-10 h-[1.375rem] shrink-0 border border-solid border-[var(--vp-input-border-color)] bg-[var(--vp-input-switch-bg-color)] transition-border-color transition-250 hover:border-[var(--vp-c-brand-1)]"
-				type="button"
-				role="switch"
-				:aria-checked="model"
-				@click="model = !model"
-			>
-				<span
-					class="absolute top-[0.09375rem] left-[0.09375rem] w-[1.125rem] h-[1.125rem] rounded-1/2 shadow-[var(--vp-shadow-1)] transition-250"
-					:class="model ? 'bg-red translate-x-[1.125rem]' : 'bg-blue'"
-				/>
-			</button>
+		</label>
+		<button
+			:id="id"
+			class="relative self-center rounded-[0.6875rem] inline w-10 h-[1.375rem] shrink-0 border border-solid border-[var(--vp-input-border-color)] bg-[var(--vp-input-switch-bg-color)] transition-border-color transition-250 hover:border-[var(--vp-c-brand-1)]"
+			type="button"
+			role="switch"
+			:aria-checked="model"
+			@click="model = !model"
+		>
+			<span
+				class="absolute top-[0.09375rem] left-[0.09375rem] w-[1.125rem] h-[1.125rem] rounded-1/2 shadow-[var(--vp-shadow-1)] transition-250"
+				:class="model
+					? ['translate-x-[1.125rem]', trueClass ?? 'bg-[var(--vp-c-brand-1)]']
+					: [falseClass ?? 'bg-[var(--vp-c-neutral-inverse)]']
+				"
+			/>
+		</button>
+		<label
+			class="cursor-pointer w-fit transition-250 transition-color"
+			:class="model ? 'text-[var(--vp-c-text-1)]' : 'text-[var(--vp-c-text-2)]'"
+			@click.stop="model = true"
+		>
 			{{ trueLabel }}
-		</div>
-	</label>
+		</label>
+	</div>
 </template>
